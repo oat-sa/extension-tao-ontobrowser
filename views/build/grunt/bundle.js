2c91012fcfc2df55939290228292a18ad089c9db
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2014-10-13 17:05:01 +0200

    dedicated compilation config

---
+++ views/build/grunt/bundle.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) { 
+
+    var requirejs   = grunt.config('requirejs') || {};
+    var clean       = grunt.config('clean') || {};
+    var copy        = grunt.config('copy') || {};
+
+    var root        = grunt.option('root');
+    var libs        = grunt.option('mainlibs');
+    var ext         = require(root + '/tao/views/build/tasks/helpers/extensions')(grunt, root);
+
+    /**
+     * Remove bundled and bundling files
+     */
+    clean.ontobrowserbundle = ['output',  root + '/ontoBrowser/views/js/controllers.min.js'];
+    
+    /**
+     * Compile tao files into a bundle 
+     */
+    requirejs.ontobrowserbundle = {
+        options: {
+            baseUrl : '../js',
+            dir : 'output',
+            mainConfigFile : './config/requirejs.build.js',
+            paths : { 'ontoBrowser' : root + '/ontoBrowser/views/js' },
+            modules : [{
+                name: 'ontoBrowser/controller/routes',
+                include : ext.getExtensionsControllers(['ontoBrowser']),
+                exclude : ['mathJax', 'mediaElement'].concat(libs)
+            }]
+        }
+    };
+
+    /**
+     * copy the bundles to the right place
+     */
+    copy.ontobrowserbundle = {
+        files: [
+            { src: ['output/ontoBrowser/controller/routes.js'],  dest: root + '/ontoBrowser/views/js/controllers.min.js' },
+            { src: ['output/ontoBrowser/controller/routes.js.map'],  dest: root + '/ontoBrowser/views/js/controllers.min.js.map' }
+        ]
+    };
+
+    grunt.config('clean', clean);
+    grunt.config('requirejs', requirejs);
+    grunt.config('copy', copy);
+
+    // bundle task
+    grunt.registerTask('ontobrowserbundle', ['clean:ontobrowserbundle', 'requirejs:ontobrowserbundle', 'copy:ontobrowserbundle']);
+};
