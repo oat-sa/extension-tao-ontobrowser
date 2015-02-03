490dd15ef06cde4bd2fe7077c1ff432da1680d99
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2015-02-03 11:23:47 +0100

    fix parrallel build

--- views/build/grunt/bundle.js
+++ views/build/grunt/bundle.js
@@ -12,7 +12,7 @@ module.exports = function(grunt) {
     /**
      * Remove bundled and bundling files
      */
-    clean.ontobrowserbundle = [out,  root + '/ontoBrowser/views/js/controllers.min.js'];
+    clean.ontobrowserbundle = [out];
     
     /**
      * Compile tao files into a bundle 
