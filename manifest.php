0a0b2e99b2d8262a2f00ad8b8561eb70206d0a44
Ionut Padurariu <ionut@taotesting.com>
2017-09-15 14:34:58 +0200

    refactoring/TAO-5105/remove_globals_from_tao_core - use constants defined in tao-core

--- manifest.php
+++ manifest.php
@@ -26,7 +26,7 @@ return array(
 	'label' => 'Model Browser',
 	'description' => 'Developement tool to browse the generis ontology',
     'license' => 'GPL-2.0',
-    'version' => '3.0.0',
+    'version' => '3.0.1',
 	'author' => 'Open Assessment Technologies',
     'requires' => array(
         'taoBackOffice' => '>=0.11'
